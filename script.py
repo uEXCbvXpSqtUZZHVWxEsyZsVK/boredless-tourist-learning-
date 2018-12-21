373dc2a5b3c740ec95422b5e204eefdb98ee0bbe
uEXCbvXpSqtUZZHVWxEsyZsVK <45900210+uEXCbvXpSqtUZZHVWxEsyZsVK@users.noreply.github.com>
2018-12-21 11:31:51 +0100

    Added test objects
    Signed-off-by: uEXCbvXpSqtUZZHVWxEsyZsVK <45900210+uEXCbvXpSqtUZZHVWxEsyZsVK@users.noreply.github.com>

---
+++ script.py
@@ -0,0 +1,5 @@
+#list of destinations
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+#test traveler
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
